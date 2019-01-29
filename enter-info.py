a1d0c849742c984ae8dfd195ac75a1d1b51b8a3b
mikekenndy <mike.kennedy.dev@gmail.com>
2019-01-28 21:26:49 -0500

    Signs waivers

--- enter-info.py
+++ enter-info.py
@@ -7,14 +7,11 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 def zenLogin():
-    # print('Email: ', end='')
-    # email = input()
-    # print('Password: ', end='')
-    # password = input()
-    # print('\nLogging in...')
-
-    email = 'obstacleathletics@gmail.com'
-    password = 'Deerpark=0'
+    print('Email: ', end='')
+    email = input()
+    print('Password: ', end='')
+    password = input()
+    print('\nLogging in...')
 
     browser = webdriver.Firefox()
     browser.get('https://studio.zenplanner.com/zenplanner/studio/index.html#/login')
@@ -35,16 +32,29 @@ def zenLogin():
     return browser
 
 def addPerson(browser, first, last, email, doc_signed):
+
+    # --- Enter personal information ---
     browser.get('https://studio.zenplanner.com/zenplanner/studio/index.html#/main/iframe/zenplanner/studio/newPerson/profile.cfm?tt=1548699725827&tt=1548700656460')
     waitForPage(browser, 'https://studio.zenplanner.com/zenplanner/studio/index.html#/main/iframe/zenplanner/studio/newPerson/profile.cfm?tt=1548699725827&tt=1548700656460')
 
-    print('Enter name')
     browser.switch_to.frame('idTheIframe')
     # Enter name
     browser.find_element_by_id('idFirstName').send_keys(first)
     browser.find_element_by_id('idLastName').send_keys(last)
     # Click next
     browser.find_element_by_css_selector('input.btn:nth-child(6)').click()
+    # Enter email address
+    browser.find_element_by_id('idPersonEmailAddress').send_keys(email)
+    # Save and Finish
+    browser.find_element_by_css_selector('input.btn:nth-child(6)').click()
+
+    # --- Sign waiver ---
+    if doc_signed:
+        browser.find_element_by_css_selector('.alertBox').click()
+        browser.find_element_by_css_selector('tr.item:nth-child(5) > td:nth-child(2) > a:nth-child(1)').click()
+        browser.find_element_by_css_selector('.err').click()
+        browser.find_element_by_css_selector('#idSignatureType_offline').click()
+        browser.find_element_by_css_selector('#idSubmit').click()
 
     return None
 
@@ -60,3 +70,4 @@ def waitForPage(browser, page):
 
 browser = zenLogin()
 addPerson(browser, 'Mike', 'Kennedy', 'email@gmail.com', True)
+print('Finished')
